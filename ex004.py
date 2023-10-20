4dd7c80b372c59d6681de1925a2abde53bee28a4
raniel54 <ranielsilva65@hotmail.com>
2023-10-20 17:26:58 -0300

    erce04

--- ex004.py
+++ ex004.py
@@ -1,2 +1,8 @@
 nome = input('Digite seu nome')
-print('Nome:' , nome)
+sobrenome = input('digite seu Sobrenome')
+idade = int(input('digite sua idade'))
+ano_nascimento = int(input('digite seu ano de nascimento'))
+maior_de_idade = input('É maior de idade?')
+altura_metros = float(input('Digite sua altura'))
+
+print('Seu Nome é {} com o sobrenome {} sua idade é {} nascido em {} É maior de idade {} com a altura de {}m'.format(nome, sobrenome, idade, ano_nascimento, maior_de_idade, altura_metros))
