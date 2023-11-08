fb4719a49c0c69608ee6e01b286527232a690470
raniel54 <ranielsilva65@hotmail.com>
2023-11-08 13:43:25 -0300

    dsdsd

---
+++ ex005.py
@@ -0,0 +1,15 @@
+#If / elif---------- / else
+# Se / se não se / se não
+entrada = input('Voce quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+  print('Voce entrou no sistema')
+  
+  print(12341234)
+  
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+else:
+    print('Voce não digitou nem entrar e nem sair.')
+
+print('FORA DOS BLOCOS')
