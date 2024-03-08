c4998658a4186a6e29e4f4630139134c1b69d266
raniel54 <ranielsilva65@hotmail.com>
2024-03-08 14:13:50 -0300

    aula7

--- aulas/aula7.py
+++ aulas/aula7.py
@@ -3,6 +3,15 @@
 # O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variavel).
 # Uso: nome_variavel = expressão
 
-raniel_oliveira = 'Raniel Oliveira da Silva'
-soma_dois_mais_dois = 2 + 2
-print(soma_dois_mais_dois, raniel_oliveira)
+#raniel_oliveira = 'Raniel Oliveira da Silva'
+#soma_dois_mais_dois = 2 + 2
+#print(soma_dois_mais_dois, raniel_oliveira)
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+
+print('Nome:', nome, 'Idade', idade)
+print('É maior? ', maior_de_idade)
+
+
