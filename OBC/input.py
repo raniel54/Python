ff05c4a82dd4406eae7a1584f4fbc83faa86562e
raniel54 <ranielsilva65@hotmail.com>
2024-03-11 16:22:42 -0300

    obc 1

---
+++ OBC/input.py
@@ -0,0 +1,12 @@
+#utilizando o Input
+
+'''
+Jogoteca
+
+'''
+
+name = input("Digite o nome do jogo\n")
+yearLaunch = int(input("Digite o ano de lançamento do jogo\n"))
+gamePrice = float(input("Digite o preço do jogo\n"))
+planincluded = bool(input("Esta incluido no plano\n"))
+print(name, yearLaunch, gamePrice, planincluded)
