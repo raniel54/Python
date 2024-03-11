ce491c74b6073e869046a20c251d80dad1b56a2d
raniel54 <ranielsilva65@hotmail.com>
2024-03-11 17:05:40 -0300

    OBC 3

---
+++ Python_EXERC/ex003.py
@@ -0,0 +1,5 @@
+x = int(input('Digite um valor: '))
+y = int(input('Digite outro valor: '))
+soma = x + y
+print('A soma entre {} e {} é igual a {}!'.format(x, y, soma))
+      
---
+++ Python_EXERC/exer2_aula12.py
@@ -0,0 +1,6 @@
+nome = 'Raniel Oliveira da Silv'
+altura = 1.81
+peso = 92.3
+imc = peso / altura ** 2
+
+print('O', nome,'tem', altura, 'de altura \n', 'pesa', peso, 'quilos e seu IMC é', imc )
