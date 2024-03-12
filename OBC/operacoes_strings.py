b5fe7e0208ae12752233e431b4367f39f43e48a7
raniel54 <ranielsilva65@hotmail.com>
2024-03-12 10:38:43 -0300

    OBC strings

---
+++ OBC/operacoes_strings.py
@@ -0,0 +1,11 @@
+gameDescription = """
+Fifa é um jogo de futebol desenvolvido pela EA Sports
+e que possibilita jogar localmente ou online
+"""
+gameName = "Fifa"
+gameVersion = "23"
+
+#1 - Operaão de concatenação de Strings
+gameFullName = gameName + gameVersion
+print(gameFullName)
+
---
+++ OBC/strings.py
@@ -0,0 +1,13 @@
+gameName = "Fifa 24"
+gameName2 = "fifa 24"
+
+print(gameName)
+print(gameName2)
+print(gameName == gameName2) #Python é case sensitive
+
+gameDescription = """
+Fifa é um jogo de futebol desenvolvido pela EA Sports
+e que possibilita jogar localmente ou online
+"""
+
+print(gameDescription)
