54117339313b41628d799c922151209202e7e161
raniel54 <ranielsilva65@hotmail.com>
2024-03-12 10:45:14 -0300

    OBC STRINGS

--- OBC/operacoes_strings.py
+++ OBC/operacoes_strings.py
@@ -4,8 +4,17 @@ e que possibilita jogar localmente ou online
 """
 gameName = "Fifa"
 gameVersion = "23"
+line = "="
 
 #1 - Operaão de concatenação de Strings
 gameFullName = gameName + gameVersion
 print(gameFullName)
 
+#2 - operação de multiplicação de Strings
+
+print(line * 25)
+
+# 3 - Procura uma palavra dentro da string
+
+print("FIfa" in gameDescription)
+print("Resident evil" in gameDescription)
