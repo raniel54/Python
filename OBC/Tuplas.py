a700eaed719222f5ec6884735602b3b1ea1c9c81
raniel54 <ranielsilva65@hotmail.com>
2024-03-14 15:26:15 -0300

    Tuplas e Listas

---
+++ OBC/Tuplas.py
@@ -0,0 +1,11 @@
+gamestuple = ("Fifa24", "Red Dead 2 ", "Star wars", "Batman Arkan knight", 
+              "Call of duty")
+print(gamestuple)
+print(type(gamestuple))
+
+# 1 - Buscar os dois primeiros itens da tupla
+print(gamestuple[:2])
+
+# - não possiblita adicionar valores na tupla
+# - não possiblita remover valores na tupla
+# - não possiblita ordenar valores na tupla
