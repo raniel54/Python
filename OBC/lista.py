7ea1c1bd92e9eedf4ffd132d9b9da8eeb4dc25e8
raniel54 <ranielsilva65@hotmail.com>
2024-03-14 15:03:25 -0300

    OBC

---
+++ OBC/lista.py
@@ -0,0 +1,23 @@
+gameFifa = ["Fifa 24", 2022, 90.50, True]
+print(gameFifa)
+
+
+gamesList = ["Resident Evil 4", "Star wars jedi Survivor", "Call of duty Modern Warfare 3", "Red Dead2"]
+print(gamesList)
+
+# 1 - Buscar os fois primeiros itens da lista
+
+print(gamesList[0:2])
+
+
+# 2 - buscar o último item da lista
+
+print(gamesList[-1])
+
+# 3 - buscar jogo até uma determinada posição
+
+print(gamesList[:3])
+
+# 4 - Buscar jogos de uma posição em diante
+
+print(gamesList[1:3])
