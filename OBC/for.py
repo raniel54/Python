a2e9457610f4b83de95a06c629644fbce1991c70
raniel54 <ranielsilva65@hotmail.com>
2024-03-15 17:26:46 -0300

    for

--- OBC/for.py
+++ OBC/for.py
@@ -11,4 +11,20 @@ for game in gamesList:
         break
     print(game)
     
-# 3 - Quando a condição for atendida 
+# 3 - Quando a condição for atendida for atendida, o loop via para a proxima iteação
+for game in gamesList:
+    if game == "Red Dead 2":
+        continue
+    print(game)
+    
+# 4 - Avaliação do jogo
+
+gameName = input("Digite o nome do jogo\n")
+gameRating = int(input("Digite quantas avaliações deja fazer no jogo\n"))
+
+sum = 0
+for i in range(gameRating):
+    note = float(input("Digite a nota para o jogo\n"))
+    sum += note # sum = sum + note
+print(f"Media de avaliação do jogo {gameName} e {sum/gameRating:.2f}")
+
