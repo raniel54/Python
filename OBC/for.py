31e43f138aea06ec93521a668b1efdb92d64ac30
raniel54 <ranielsilva65@hotmail.com>
2024-03-15 13:16:05 -0300

    for

---
+++ OBC/for.py
@@ -0,0 +1,14 @@
+gamesList = ["Fifa", "God of war", "Red Dead 2", "Uncharted", 90.5]
+
+# 1 - Iterando valores de uma lista
+for game in gamesList:
+     print(game)
+     
+# 2 - Quando a condição for aytndida, o loop seá encerrado
+
+for game in gamesList:
+    if game == "Red Dead 2":
+        break
+    print(game)
+    
+# 3 - Quando a condição for atendida 
