9d4fc4aa4678956e8870635575b6c81e00e38117
Raniel <ranielsilva65@hotmail.com>
2024-03-23 22:42:11 -0300

    Exer 4 modulo

---
+++ OBC_Modulo/Exer/exer_4.py
@@ -0,0 +1,46 @@
+## Advinhe o Número
+
+# Escreva um programa em Python que gera um número aleatório para que o usuário tente acertar o número. Algumas sugestões para o programa:
+
+# 1. Utilizar um laço de repetição para que o programa execute até que o usuário informe um número referente a opção para sair do programa.
+# 2. Utilizar o módulo random para gerar valores aleatórios dentro de um intervalo. Ex: 1 a 10.
+# 3. Lê o número que o usuário digitar via input e comparar se é o mesmo número que o programa sorteou.
+
+import random
+
+# check_numbers = int(input("Digite um numero de 1 a 10\n"))
+# r1 = random.randint(1, 10)
+# while check_numbers != r1:
+#         print(f"Voce errou o numero tente novamente. ")
+#         check_numbers = int(input("Digite um numero de 1 a 10\n"))
+         
+# else:
+#     print(f"Parabens voce acertou o numero. Seu numero foi {check_numbers} e o sorteado foi {r1}\n")
+    
+done = False
+
+while not done:
+    print("O que você deseja fazer?")
+    print("1. Advinhar o número")
+    print("2. Sair")
+
+    choice = input("> ")
+
+    if choice == "1":
+        print("=====Advinhe um número de 1 a 10======\n")
+        number = int(input("Digite um número de 1 a 10\n"))
+        result = random.randint(1, 10)
+        if number == result:
+            print("Parabéns. Você acertou!")
+        else:
+            print(f"Tente novamente. O número sorteado foi {result}")
+    elif choice == "2":
+        done = True
+    else:
+        print("Opção inválida. Escolha uma opção entre 1 e 2")
+
+
+           
+  
+    
+
