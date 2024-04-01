0cc35602e38f0167bb37d0efe6a9e9b61b7b456e
raniel54 <ranielsilva65@hotmail.com>
2024-04-01 10:56:07 -0300

    exer2

--- POO/Exer/exer2.py
+++ POO/Exer/exer2.py
@@ -6,6 +6,7 @@
 # 2. A classe deve ter um método construtor e o método dundle str.
 # 3. A classe deve ter um método para desconto. O método deve receber o desconto em percentual e realizar o cálculo de quanto ficaria o preço final com o desconto.
 
+#Meu Codigo
 class produto:
     def __init__(self, name, sabor, ml) -> None:
         self.name = name
@@ -19,15 +20,8 @@ class produto:
     def __str__(self) -> str:
         return f"Produto: {self.name}"
     
-    def informacoes(self):
-        print("##Informações do Produto##")
-        print(F"Produto:{self.name}\n")
-        print(F"Preço:{self.valor}\n")
-        print(F"valor do desconto: {self.dp}\n")
-        
-    
-    def des(self, nota):
-        self.dp += nota
+    def des(self, desconto):
+        self.dp += desconto
         self.desconto = self.valor * self.dp/100
         
     def media(self):
@@ -36,3 +30,25 @@ class produto:
 monster = produto("MONSTER ENERGY JUICE PIPELINE PUNCH","Maracujá, Laranja e Goiaba", 375)
 monster.des(20)
 monster.media()
+
+
+#Codigo do professor
+# class Product:
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+
+#     def __str__(self):
+#         return f"Produto: {self.name} - R$ {self.price} reais"
+
+#     def discount(self, perc_discount):
+#         valorDiscount = (self.price/100) *perc_discount
+#         finalPrice = self.price - valorDiscount
+#         return int(finalPrice)
+    
+# xbox = Product("Xbox One", 4000)
+# print(xbox)
+# print(xbox.discount(20))
+# iphone = Product("Iphone 14", 8000)
+# print(iphone)
+# print(iphone.discount(10))
